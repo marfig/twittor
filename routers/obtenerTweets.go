2ab91b7adcc369bbf0c9350b4f04b607c69171e5
Marvin Figueredo <marvin.elder7@gmail.com>
2021-04-20 17:15:40 -0400

    Se agregan endpoints para agregar y listar tweets

--- routers/obtenerTweets.go
+++ routers/obtenerTweets.go
@@ -1,7 +1,38 @@
 package routers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
 
-func ObtenerTweets(w http.ResponseWriter, r *http.Response) {
+	"github.com/marfig/twittor/bd"
+)
 
+func ObtenerTweets(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "Debe enviar el parámetro ID", http.StatusBadRequest)
+		return
+	}
+
+	if len(r.URL.Query().Get("pagina")) < 1 {
+		http.Error(w, "Debe enviar el parámetro página", http.StatusBadRequest)
+		return
+	}
+	pagina, err := strconv.Atoi(r.URL.Query().Get("pagina"))
+	if err != nil {
+		http.Error(w, "El parámetro página debe ser mayor a 0", http.StatusBadRequest)
+		return
+	}
+
+	pag := int64(pagina)
+	respuesta, correcto := bd.ObtenerTweets(ID, pag)
+	if correcto == false {
+		http.Error(w, "Error al leer los tweets", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(respuesta)
 }
